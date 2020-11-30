38de392166655ace892a4d73610b8c43a6126c55
starod00m <nikita@fedosov.net>
2020-11-30 08:15:55 +0500

    Initial commit

---
+++ parser.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Ya.Music parser')
+
+parser.add_argument('url', action="store", help='Ya.Music url to parse')
+parser.add_argument('-p', '--path', dest='path', default='chromedriver.exe', help='Path to chromedriver.exe')
+
+args = parser.parse_args()
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+browser = webdriver.Chrome(options=options, executable_path=args.path)
+
+browser.get(args.url)
+time.sleep(1)
+browser.find_element_by_css_selector('div.payment-plus__header-close span.d-icon_cross-big').click()
+
+tracks = []
+is_end = False
+while True:
+    if not is_end:
+        is_end = True
+        time.sleep(0.5)
+        elements = browser.find_elements_by_css_selector('div.typo-track div.d-track__overflowable-wrapper')
+        for element in elements:
+            track = ' '.join(element.text.split('\n'))
+            if track not in tracks:
+                tracks.append(track)
+                is_end = False
+        time.sleep(0.5)
+        browser.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+        browser.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+    else:
+        break
+with open('result.txt', 'w', encoding='utf-8') as f:
+    for track in tracks:
+        f.write(track + '\n')
+browser.close()
---
+++ spotify_liker.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Ya.Music parser')
+
+parser.add_argument('login', action="store", help='login')
+parser.add_argument('password', action="store", help='password')
+parser.add_argument('-p', '--path', dest='path', default='chromedriver.exe', help='Path to chromedriver.exe')
+parser.add_argument('-f', '--file', dest='file', default='result.txt', help='Path to result.txt')
+
+args = parser.parse_args()
+
+def get_tracks() -> list:
+    tracks = []
+    with open(args.file, 'r', encoding='utf-8') as f:
+        for line in f:
+            tracks.append(' '.join(line.split()))
+    return tracks
+
+tracks = get_tracks()
+print(tracks)
+browser = webdriver.Chrome(executable_path=args.path)
+browser.get('https://accounts.spotify.com/en/login?continue=https:%2F%2Fopen.spotify.com%2F')
+time.sleep(1)
+browser.find_element_by_id('login-username').send_keys(args.login)
+browser.find_element_by_id('login-password').send_keys(args.password)
+browser.find_element_by_id('login-button').click()
+time.sleep(1)
+browser.get('https://open.spotify.com/search')
+time.sleep(1)
+for i, track in enumerate(tracks):
+    if i + 1 % 10 == 0:
+        time.sleep(10)
+    try:
+        browser.find_element_by_css_selector('input[data-testid]').clear()
+        time.sleep(1)
+        browser.find_element_by_css_selector('input[data-testid]').send_keys(track)
+        time.sleep(1)
+        browser.find_element_by_css_selector('div[data-testid="tracklist-row"]').click()
+        time.sleep(1)
+        try:
+            browser.find_element_by_css_selector('div[aria-rowindex="1"] button[aria-checked="false"]').click()
+        except NoSuchElementException:
+            msg = f'Track {track} already liked'
+            print(msg)
+            with open('already_liked.txt', 'a', encoding='utf-8') as f:
+                f.write(msg + '\n')
+            continue
+        time.sleep(1)
+    except NoSuchElementException:
+        msg = f'Can\'t like {track}'
+        print(msg)
+        with open('cant like.txt', 'a', encoding='utf-8') as f:
+            f.write(msg + '\n')
+        continue
+
+browser.close()
